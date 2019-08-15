c38e63f59b3023df83184c2b5ae391a3c4d7642a
arshita.gupta <arshitagupta27@gmail.com>
2019-08-15 06:30:12 -0400

    updated helper_rle_mask

--- src/helper_rle_mask.py
+++ src/helper_rle_mask.py
@@ -1,7 +1,25 @@
+import argparse
 import csv
 import cv2
+import numpy as np
+import os
 
 from mask_functions import rle2mask
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-t', '--root_dir',
+#                     default="/media/arshita/Windows/Users/arshi/Desktop/Project2")
+#                     # description="")
+# parser.add_argument('-t', '--mask_folder',
+#                     default="mask_new3")
+#                     # description="")
+# parser.add_argument('-t', '--csv_file',
+#                     default="train-rle.csv")
+#                     # description="")
+#
+# args = parser.parse_args()
+
+
 
 def get_masks_from_rle(csv_file_name, mask_file_name):
     """
@@ -52,6 +70,41 @@ def get_masks_from_rle(csv_file_name, mask_file_name):
                     print("Old Image re-written in folder")
 
 
+def no_pnemo_mask(root):
+    """
+    A short description.
+
+    A bit longer description.
+
+    Args:
+        variable (type): description
+
+    Returns:
+        type: description
+
+    Raises:
+        Exception: description
+
+    """
+    imgs_list = list(sorted(os.listdir(os.path.join(root, "SIIM_png_train"))))
+    masks_list = list(sorted(os.listdir(os.path.join(root, "mask_new3"))))
+    if imgs_list == masks_list:
+        print("Equal image and mask files")
+    else:
+        for img_name in imgs_list:
+            if img_name not in masks_list:
+                print("Writing blank image")
+                create_blank_image = np.zeros((1024,1024,1), np.uint8)
+                name = root + "/mask_new3/" + img_name
+                cv2.imwrite(name,create_blank_image)
+
+
 if __name__ == "__main__":
-    get_masks_from_rle('/home/arshita/Desktop/project_2/siim-acr-pneumothorax-segmentation/SIIM/train-rle.csv',
-                        '/media/arshita/Windows/Users/arshi/Desktop/Project2/mask_new')
+    root = "/media/arshita/Windows/Users/arshi/Desktop/Project2"
+    mask_folder = "mask_new3"
+    csv_file = "/home/arshita/Desktop/project_2/siim-acr-pneumothorax-segmentation/SIIM/train-rle.csv"
+    get_masks_from_rle(csv_file, os.path.join(root, mask_folder))
+    no_pnemo_mask(root)
+
+    # get_masks_from_rle(args.csv_file, os.path.join(args.root, args.mask_folder))
+    # no_pnemo_mask(args.root)
